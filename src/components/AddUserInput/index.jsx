1b8fd775f5bc2d06bd06754c99688ed9e1c7bab9
Диана Ким Александровна <dkim191108@gmail.com>
2025-07-29 13:46:16 +0500

    email validation

--- src/components/AddUserInput/index.jsx
+++ src/components/AddUserInput/index.jsx
@@ -5,23 +5,49 @@ import './style.css';
 const AddUserInput = ({ emailList, setEmailList }) => {
     const [email, setEmail] = useState("");
 
+    const symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@._";
+
+    const isValidEmail = (email) => {
+        for (let i = 0; i < email.length; i++) {
+            if (!symbols.includes(email[i])) {
+                return false;
+            }
+        }
+
+        const atCount = email.split("@").length - 1;
+        if (atCount !== 1) return false;
+
+        const [local, domain] = email.split("@");
+        if (!domain || !domain.includes(".")) return false;
+
+        const allowedDomains = ["@gmail.com", "@mail.com"];
+        const isAllowedDomain = allowedDomains.some(domain => email.endsWith(domain));
+        if (!isAllowedDomain) return false;
+
+        return true;
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        if (!email.includes("@gmail.com") && !email.includes("@mail.com")) {
+        if (!isValidEmail(email)) {
+            alert("Некорректный email.");
             return;
         }
 
         setEmailList([...emailList, { email, active: true }]);
-        setEmail("");
+        setEmail(""); 
     };
 
     return (
         <div className="email-container-header">
             <div className="user-container">
                 <span>Добавить пользователя</span>
-                <a href='#' className="link"><SvgSelector name={'iconLink'} />Скопировать</a>
+                <a href="#" className="link">
+                    <SvgSelector name={'iconLink'} />Скопировать
+                </a>
             </div>
+
             <form className="add-email" onSubmit={handleSubmit}>
                 <input
                     type="text"
@@ -29,8 +55,8 @@ const AddUserInput = ({ emailList, setEmailList }) => {
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
-                <button className="btn" type="submit" onSubmit={handleSubmit}>
-                    <SvgSelector name={'iconPerson'} action={() => console.log("click")} />
+                <button className="btn" type="submit">
+                    <SvgSelector name={'iconPerson'} />
                     <span className="btn-text">Пригласить</span>
                 </button>
             </form>
